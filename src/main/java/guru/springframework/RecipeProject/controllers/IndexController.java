c4f25a8967c0b2c5dab7f590ef6e1180e906db70
Max <max.juarez625@gmail.com>
2022-03-06 22:26:26 -0500

    Assignment Review: Display List of Recipes

--- src/main/java/guru/springframework/RecipeProject/controllers/IndexController.java
+++ src/main/java/guru/springframework/RecipeProject/controllers/IndexController.java
@@ -1,33 +1,24 @@
 package guru.springframework.RecipeProject.controllers;
 
-import guru.springframework.RecipeProject.domain.Category;
-import guru.springframework.RecipeProject.domain.unitOfMeasure;
-import guru.springframework.RecipeProject.repositories.CategoryRepository;
-import guru.springframework.RecipeProject.repositories.unitMeasureRepository;
+import guru.springframework.RecipeProject.services.RecipeService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-
-import java.util.Optional;
 
 @Controller
 public class IndexController {
 
-    private CategoryRepository categoryRepository;
-    private unitMeasureRepository unitMeasureRepository;
 
-    public IndexController(CategoryRepository categoryRepository, unitMeasureRepository unitMeasureRepository) {
-        this.categoryRepository = categoryRepository;
-        this.unitMeasureRepository = unitMeasureRepository;
+    private final RecipeService recipeService;
+
+    public IndexController(RecipeService recipeService) {
+        this.recipeService = recipeService;
     }
 
     @RequestMapping({"","/","/index"})
-    public String getIndex(){
+    public String getIndex(Model model){
 
-        Optional<Category> categoryOptional = categoryRepository.findByDescription("American");
-        Optional<unitOfMeasure> unitOfMeasureOptional = unitMeasureRepository.findByDescription("Teaspoon");
-
-        System.out.println("cat ID is: " +categoryOptional.get().getId());
-        System.out.println("Unit of Measure ID is: " +unitOfMeasureOptional.get().getId());
+        model.addAttribute("recipes",recipeService.getRecipes());
 
         return "index";
     }
--- src/main/java/guru/springframework/RecipeProject/services/RecipeServiceImpl.java
+++ src/main/java/guru/springframework/RecipeProject/services/RecipeServiceImpl.java
@@ -2,10 +2,12 @@ package guru.springframework.RecipeProject.services;
 
 import guru.springframework.RecipeProject.domain.Recipe;
 import guru.springframework.RecipeProject.repositories.RecipeRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Service
 public class RecipeServiceImpl implements RecipeService{
 
     private final RecipeRepository recipeRepository;
